701f0352781f694746e66b8197ed2e232bf68f8c
inoy <7m7110123@gmail.com>
2021-05-17 18:10:11 +0900

    Why Tuples?

--- features/tuples.ts
+++ features/tuples.ts
@@ -10,3 +10,11 @@ const pepsi: Drink = ['brown', true, 40];
 // pepsi[0] = 40; これはTSがエラーにしてくれる
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+// meaningless
+const carSpecs: [number, number] = [400, 3354];
+// meaningful
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
